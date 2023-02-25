33004391bffc9d9c326af838848ed48fbc327e11
Muhammed Semih <muhammedsemihekmen@hotmail.com>
2023-02-25 20:46:28 +0300

    Initial Commit

---
+++ UICreateWithoutStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICreateWithoutStoryBoard
+//
+//  Created by Semih Ekmen on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
